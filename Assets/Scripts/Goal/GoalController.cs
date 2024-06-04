afd2e17664af540c19d369ed6dcb280fd335b9b9
lutfiizzat <lutfi.izzat3@gmail.com>
2024-06-04 20:05:20 +0800

    added audio clip when cube is in goal

--- Assets/Scripts/Goal/GoalController.cs
+++ Assets/Scripts/Goal/GoalController.cs
@@ -8,9 +8,14 @@ public class GoalController : MonoBehaviour
     [SerializeField] public GameObject Block;
     [NonSerialized] public bool IsFulfilled = false;
     [NonSerialized] private string blockTag;
+    [SerializeField] public AudioClip goalSound;
+    public AudioSource audioSource;
+    [NonSerialized] private bool hasPlayedSound = false;
+
     void Start()
     {
         blockTag = Block.tag;
+        audioSource.clip = goalSound;
     }
 
     void Update()
@@ -18,16 +23,27 @@ public class GoalController : MonoBehaviour
 
     }
 
-    void OnTriggerStay(Collider other) {
-        if (other.tag == blockTag) {
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == blockTag)
+        {
+            if (!hasPlayedSound)
+            {
+                audioSource.Play();
+                hasPlayedSound = true;
+            }
             IsFulfilled = true;
             Debug.Log("IN");
         }
     }
 
-    void OnTriggerExit(Collider other) {
-        if (other.tag == blockTag) {
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == blockTag)
+        {
             IsFulfilled = false;
+            hasPlayedSound = false; // Reset the flag when the object leaves the trigger
         }
     }
 }
+
